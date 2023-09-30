11aa78416e3b89a0be9604f01c4da3180a25e860
Allan Albuquerque <allan@xmiiconsulting.com>
2023-09-30 19:16:25 -0300

    renamed:    app.env -> dev.local.env

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,16 +8,22 @@ import { PractitionerModule } from './practitioner/practitioner.module';
 import { OrganizationModule } from './organization/organization.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Employee } from './app.entity';
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     HttpModule.register({
       timeout: 5000, // Optional: Configure your timeout settings
     }),
     TypeOrmModule.forRootAsync({
       useFactory: () => ({
         type: 'mssql',
-        url: process.env.DATABASE_URL,
+        host: 'fhir-care-link.database.windows.net',
+        port: 1433,
+        database: 'CareLink',
+        username: 'fhiruser',
+        password: 'CareLink23*',
         synchronize: true, // For development only; consider setting to false in production
         entities: ['dist/**/*.entity{.ts,.js}'],
       }),
