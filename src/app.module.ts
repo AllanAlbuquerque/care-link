bd1319cc5d7893999b59ebd20498112f5c5db99a
Allan Albuquerque <allan@xmiiconsulting.com>
2023-09-30 18:16:32 -0300

    database created

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,25 @@ import { PatientModule } from './patient/patient.module';
 import { HttpModule } from '@nestjs/axios';
 import { PractitionerModule } from './practitioner/practitioner.module';
 import { OrganizationModule } from './organization/organization.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Employee } from './app.entity';
 
 @Module({
   imports: [
     HttpModule.register({
       timeout: 5000, // Optional: Configure your timeout settings
     }),
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      database: 'CareLink',
+      port: 1433,
+      host: 'fhir-care-link.database.windows.net',
+      username: 'fhiruser',
+      password: 'CareLink23*',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([Employee]),
     PatientModule,
     PractitionerModule,
     OrganizationModule,
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Employee } from './app.entity';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @InjectRepository(Employee)
+    private employeesRepository: Repository<Employee>,
+  ) {}
+
+  getHello(): Promise<Employee[]> {
+    return this.employeesRepository.find();
   }
 }
