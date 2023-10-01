1e1fa4e181bb07f15b94d34798b769dd4b41172d
Allan Albuquerque <allan@xmiiconsulting.com>
2023-10-01 01:22:16 -0300

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,6 +16,7 @@ import { ConfigModule } from '@nestjs/config'
     HttpModule.register({
       timeout: 5000, // Optional: Configure your timeout settings
     }),
+    //kk
     TypeOrmModule.forRootAsync({
       useFactory: () => ({
         type: 'mssql',
